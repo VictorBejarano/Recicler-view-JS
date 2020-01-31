a66d25901d17c21db0ac185ad9ab2ea630a42d7c
victor bejarano <alejandrobjrn4@gmail.com>
2020-01-31 09:26:50 -0500

    Test OOP

--- scripts/WindowView.js
+++ scripts/WindowView.js
@@ -1,3 +1,12 @@
 class WindowView {
-    
+    constructor(_width,_height) {
+        this._width = _width;
+        this._height = _height;
+        this._center = [this._width / 2,this._height /2];
+    }
+
+    printData() {
+        console.log(this._center)
+    }
+
 }
---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    var windowView = new WindowView($(document).width(),$(document).height(),[2,3]);
+    windowView.printData();
+});
+
