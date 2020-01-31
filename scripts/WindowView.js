8c874abc0aafdf03dafa43973cf6170bbb36d996
victor bejarano <alejandrobjrn4@gmail.com>
2020-01-31 10:44:45 -0500

    Angular OOP

--- scripts/WindowView.js
+++ scripts/WindowView.js
@@ -1,12 +1,31 @@
 class WindowView {
-    constructor(_width,_height) {
+/*    constructor(_width,_height) {
         this._width = _width;
         this._height = _height;
         this._center = [this._width / 2,this._height /2];
+    }*/
+    //Setters
+    set setWidth (_width) {
+        this._width = _width;
     }
-
+    set setHeigth (_heigth) {
+        this._heigth = _heigth;
+    }
+    //Getters
+    get getCenter(){
+        this._center = [this._width / 2,this._height /2];
+        return  this._center;
+    }
     printData() {
-        console.log(this._center)
+        app.config(function($routeProvider) {
+            $routeProvider
+            .when("/banana", {
+                templateUrl : "/Pages/page1.html"
+            })
+            .when("/tomato", {
+                template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
+            });
+        });
     }
 
 }
--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,10 @@
+var windowView = new WindowView();
 $(document).ready(function(){
-    var windowView = new WindowView($(document).width(),$(document).height(),[2,3]);
-    windowView.printData();
+    //windowView = new WindowView($(document).width(),$(document).height(),[2,3]);
+    windowView.setWidth = $(document).width();
+    windowView.setHeigth = $(document).height();
 });
-
 var app = angular.module("myApp", ["ngRoute"]);
-app.config(function($routeProvider) {
-    $routeProvider
-    .when("/banana", {
-        template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
-    })
-    .when("/tomato", {
-        template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
-    });
-});
+windowView.printData();
+alert(windowView.getCenter)
+
