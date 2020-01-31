43ef372ce550ef11089b81b1fce03b7ffe7eb1fe
victor bejarano <alejandrobjrn4@gmail.com>
2020-01-31 09:55:32 -0500

    Test Angular Router

--- scripts/index.js
+++ scripts/index.js
@@ -3,3 +3,13 @@ $(document).ready(function(){
     windowView.printData();
 });
 
+var app = angular.module("myApp", ["ngRoute"]);
+app.config(function($routeProvider) {
+    $routeProvider
+    .when("/banana", {
+        template : "<h1>Banana</h1><p>Bananas contain around 75% water.</p>"
+    })
+    .when("/tomato", {
+        template : "<h1>Tomato</h1><p>Tomatoes contain around 95% water.</p>"
+    });
+});
