16440cf68cc8284138e04e4e48e09b5ecb50be34
Vikthor <alejandrobjrn4@gmail.com>
2020-02-11 10:35:49 -0500

    Clear code

--- scripts/index.js
+++ scripts/index.js
@@ -27,45 +27,6 @@ $(document).ready(function(){
     });
     
     console.log(containerView.getCenterPosition[1])
-    containerView.printData(2);
-    
-    // $("#containerView").position().top
-    // $('.triangle-right')
-    //     .css({
-    //         'position':'fixed',
-    //         'left': '2vw',
-    //         'top': (containerView.getHeigth + $("#containerView").position().top) + 'px',
-    //         'width': '0',
-    //         'height': '0',
-    //         'border-top': '2vw solid transparent',
-    //         'border-left': '4vw solid #555',
-    //         'border-bottom': '2vw solid transparent'
-    //     });
-
-    // $('.triangle-left')
-    //     .css({
-    //         'position':'fixed',
-    //         'left': '2vw',
-    //         'top': '0px',
-    //         'width': '0',
-    //         'height': '0',
-    //         'border-top': '2vw solid transparent',
-    //         'border-right': '4vw solid #555',
-    //         'border-bottom': '2vw solid transparent'
-    //     });        
+    containerView.printData(2);      
 });
-// var app = angular.module("myApp", ["ngRoute"]);
-// var number = 1;
-
-// containerView.printData(2);
-// function prueba(){
-//     $(document).ready(function(){
-//         containerView.loadView('#containerView','Pages','page');
-//     });
-// }
-// function prueba2() {
-//     $(document).ready(function(){
-//         console.log($("#containerView").position())
-//     });
-// }
 
